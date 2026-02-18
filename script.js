dedc159c5ef4b9e8717ab617cd88f5a164edcbf6
PC 2 <ghost1wq3@gmail.com>
2026-02-18 21:04:45 +0530

    Added the mandatory public discloser

--- script.js
+++ script.js
@@ -208,48 +208,7 @@ items.forEach(item => {
     item.style.transform = "translateY(0)";
   });
 });
-/* =========================
-   GLIMPSE PARALLAX
-========================= */
 
-const glimpseCards = document.querySelectorAll(".glimpse-card");
-
-glimpseCards.forEach(card => {
-
-  card.addEventListener("mousemove", e => {
-
-    const rect = card.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-
-    const centerX = rect.width / 2;
-    const centerY = rect.height / 2;
-
-    const rotateX = -(y - centerY) / 20;
-    const rotateY = (x - centerX) / 20;
-
-    card.style.transform = `
-      perspective(900px)
-      rotateX(${rotateX}deg)
-      rotateY(${rotateY}deg)
-      scale(1.05)
-    `;
-
-    const img = card.querySelector("img");
-    img.style.transform = `
-      translateX(${(x - centerX)/25}px)
-      translateY(${(y - centerY)/25}px)
-      scale(1.1)
-    `;
-  });
-
-  card.addEventListener("mouseleave", () => {
-    card.style.transform = "scale(1)";
-    const img = card.querySelector("img");
-    img.style.transform = "scale(1)";
-  });
-
-});
 /* Footer Reveal Animation */
 const footerItems = document.querySelectorAll(".reveal-footer");
 
